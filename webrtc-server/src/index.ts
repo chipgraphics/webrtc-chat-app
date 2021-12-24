36d43a1e2b7b682abf9e2c17d590877444485d71
kadircan-yalniz <kadircanyz69@gmail.com>
2021-12-24 23:03:52 +0300

    base arch server

--- webrtc-server/src/index.ts
+++ webrtc-server/src/index.ts
@@ -1,1 +1,43 @@
-console.log("hellof rom typesciofd");
+import http from "http";
+import path from "path";
+
+import express from "express";
+import socketio, { Socket } from "socket.io";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config({ path: __dirname + "/../.env" });
+
+const server = http.createServer(app);
+
+app.use(express.json());
+//app.use(express.static("public"));
+const io = new socketio.Server(server);
+let userCount = 0;
+
+io.on("connection", (socket: Socket) => {
+  if (userCount === 2) {
+    return;
+  }
+  socket.emit("initConnect", userCount);
+  userCount++;
+
+  socket.on("sendOffer", (sdp: RTCSessionDescriptionInit) => {
+    socket.broadcast.emit("handleOffer", sdp);
+  });
+  socket.on("sendAnswer", (sdp: RTCSessionDescriptionInit) => {
+    socket.broadcast.emit("handleAnswer", sdp);
+  });
+  socket.on("sendCandidate", (candidate: RTCIceCandidateInit) => {
+    socket.broadcast.emit("getCandidate", candidate);
+  });
+
+  socket.on("disconnected", () => {
+    // Is it correct to do anything here ? WebRTC must be configured between peerse.
+    //You can specify what client side do when other user exit
+    userCount--;
+  });
+});
+server.listen(process.env.PORT, () => {
+  console.log("listening port:", process.env.PORT);
+});
