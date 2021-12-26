a9ba8b1047820fc4a2eca069d8d0bf8ae5c36bb2
kadircan-yalniz <kadircanyz69@gmail.com>
2021-12-26 06:21:35 +0300

    Cli draft added

---
+++ webrtc-cli/src/cli.ts
@@ -0,0 +1,84 @@
+import io from "socket.io-client";
+import { Command } from "commander";
+import readline from "readline";
+import Peer from "simple-peer";
+//@ts-ignore
+import wrtc from "wrtc";
+//import Buffer from "buffer/";
+const program = new Command();
+program.version("0.0.1");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function connect() {
+  const socket = io("http://localhost:8000");
+  let peer: Peer.Instance;
+  socket.emit("join room", () => {});
+  socket.on("all users", (users) => {
+    peer = createPeer(users[0], socket.id);
+  });
+  socket.on("user joined", (payload) => {
+    console.log("user joined");
+    peer = addPeer(payload.signal, payload.callerID);
+  });
+  socket.on("receiving returned signal", (payload) => {
+    console.log("receiving returned signal");
+    peer.signal(payload.signal);
+  });
+  socket.on("room full", () => {
+    console.log("room is full");
+  });
+  socket.on("user left", () => {
+    console.log("Peer disconnected");
+    peer.destroy();
+  });
+  rl.on("line", (message) => {
+    console.log(peer.connected);
+    if (peer.connected === true) {
+      peer.write(message);
+    }
+  });
+  function createPeer(userToSignal: any, callerID: string) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      wrtc: wrtc,
+    });
+    peer.on("signal", (signal) => {
+      socket.emit("sending signal", { userToSignal, callerID, signal });
+    });
+    peer.on("data", (data) => {
+      let buf = Buffer.from(data);
+      console.log(`Peer: ${buf.toString()}`);
+    });
+    peer.on("close", () => {
+      console.log("Closed");
+      peer.destroy();
+    });
+    return peer;
+  }
+  function addPeer(incomingSignal: any, callerID: string) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      wrtc: wrtc,
+    });
+    peer.on("signal", (signal) => {
+      socket.emit("returning signal", { signal, callerID });
+    });
+    peer.on("data", (data) => {
+      let buf = Buffer.from(data);
+      console.log(`Peer: ${buf.toString()}`);
+    });
+
+    peer.on("close", () => {
+      console.log("Closed");
+      peer.destroy();
+    });
+    peer.signal(incomingSignal);
+    return peer;
+  }
+}
+connect();
---
+++ webrtc-server/src/server.ts
@@ -0,0 +1,56 @@
+import http from "http";
+import path from "path";
+
+import express from "express";
+import socketio, { Socket } from "socket.io";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config({ path: __dirname + "/../.env" });
+
+const server = http.createServer(app);
+
+app.use(express.json());
+//app.use(express.static("public"));
+const io = new socketio.Server(server);
+let userCount = 0;
+const socketToRoom = {};
+const users: string[] = [];
+io.on("connection", (socket: Socket) => {
+  socket.on("join room", () => {
+    const length = users.length;
+    if (length === 2) {
+      socket.emit("room full");
+      return;
+    }
+    users.push(socket.id);
+    const usersInThisRoom = users.filter((id) => id !== socket.id);
+    console.log(users);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+  console.log("new connection");
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+  socket.on("disconnect", () => {
+    let index = users.indexOf(socket.id);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+    socket.broadcast.emit("user left");
+  });
+});
+server.listen(process.env.PORT, () => {
+  console.log("listening port:", process.env.PORT);
+});
